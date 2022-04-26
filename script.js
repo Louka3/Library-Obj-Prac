4c4dac0906ff91db19747300819089d09073004d
Louka3 <lkuczykowski@gmail.com>
2022-04-26 17:49:19 -0400

    Created Book object. Contains 'info' method

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ function Book(title, author, pages, read){
     this.author = author
     this.pages = pages
     this.read = read
+
+    
     this.info = function(){
-        
+        return title + " by " + author + ", " + pages + " pages, " +
+        read + "."
     }
 }
+const theWayOfKings = new Book("The Way of Kings", "Brandon Sanderson",
+    "1007", "already read")
+
+console.log(theWayOfKings.info())
