10688cd8d3ec36cbdbb704234bfe6eead061fdc8
Louka3 <lkuczykowski@gmail.com>
2022-04-26 18:17:14 -0400

    Added 2 books. Put them into an array

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-
-    
+  
     this.info = function(){
         return title + " by " + author + ", " + pages + " pages, " +
         read + "."
     }
 }
+ Book.prototype.addBookToLibrary = function(book){
+    myLibrary.push(book)
+ }
+
+
 const theWayOfKings = new Book("The Way of Kings", "Brandon Sanderson",
     "1007", "already read")
 
+const wordsOfRadiance = new Book("Words of Radiance", "Brandon Sanderson",
+    "1087", "already read")
+
+theWayOfKings.addBookToLibrary(theWayOfKings)
+wordsOfRadiance.addBookToLibrary(wordsOfRadiance)
+
 console.log(theWayOfKings.info())
+console.log(wordsOfRadiance.info())
+console.log(myLibrary)
+
+
+
