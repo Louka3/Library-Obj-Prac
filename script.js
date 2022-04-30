5fd53b2d6ff36602f44d109b65157b254e462cc8
Louka3 <lkuczykowski@gmail.com>
2022-04-30 18:11:49 -0400

    Finished making the 'Add Book' button. Creates new book, adds to library array, and displays on HTML

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function Book(title, author, pages, readStatus){
     myLibrary.push(this)
 }
 
-
 let theWayOfKings = new Book("The Way of Kings", "Brandon Sanderson",
     "1007", "already read")
 
@@ -35,12 +34,12 @@ console.log(wordsOfRadiance.info())
 console.log(myLibrary)
 
 function createNewBook(){
-    let newTitle = input("What is the title of the book?")
-    let newAuthor = input("Who is the author of the book?")
-    let newPageCount = input("How many pages are in the book?")
-    let newReadStatus = input("Have you already read this book?('already read' or 'have not read yet')")
-    let addedBook = new Book(newTitle, newAuthor, newPageCount, newReadStatus)
-    addedBook.addBookToLibrary()
+    let newTitle = prompt("What is the title of the book?")
+    let newAuthor = prompt("Who is the author of the book?")
+    let newPageCount = prompt("How many pages are in the book?")
+    let newReadStatus = prompt("Have you already read this book?('already read' or 'have not read yet')")
+    let newBook = new Book(newTitle, newAuthor, newPageCount, newReadStatus)
+    return newBook
 }
 
 function bookToLibraryLoop(bookToAdd){
@@ -69,35 +68,15 @@ function bookToLibraryLoop(bookToAdd){
     libraryContainer.appendChild(newBook)
 }
 
+function bookToLibraryEvent(){
+    let newBook = createNewBook()
+    newBook.addBookToLibrary()
+    bookToLibraryLoop(newBook)
+}
+
 for (let i = 0; i < myLibrary.length; i++){
     bookToLibraryLoop(myLibrary[i])
 }
 
-// for (let i = 0; i < myLibrary.length; i++){
-//     let newBook = document.createElement('div') //create elements
-//     let bookTitle = document.createElement('p')
-//     let bookAuthor = document.createElement('p')
-//     let bookPages = document.createElement('p')
-//     let bookReadStatus = document.createElement('p')
-
-//     newBook.className = "new-book" //give elements their class names
-//     bookTitle.className = "book-title"
-//     bookAuthor.className = "author"
-//     bookPages.className = "page-count"
-//     bookReadStatus.className = "read-status"
-    
-//     bookTitle.textContent = myLibrary[i].title //add the object attributes to the elements
-//     bookAuthor.textContent = myLibrary[i].author
-//     bookPages.textContent = myLibrary[i].pages
-//     bookReadStatus.textContent = myLibrary[i].readStatus
-    
-//     let newButton = document.createElement('button') //the read-status button
-//     newButton.className = "read-status-button"
-//     newButton.innerText = "Read Status"
-    
-//     newBook.append(bookTitle, bookAuthor, bookPages, bookReadStatus, newButton) //put it all together and add to library
-//     libraryContainer.appendChild(newBook)
-// }
-
 let addBookButton = document.querySelector('.add-book-button')
-addBookButton.addEventListener('click',createNewBook)
+addBookButton.addEventListener('click',bookToLibraryEvent)
